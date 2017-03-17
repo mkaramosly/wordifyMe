033358d940b0a1664be4bb45fbf431b59842503d
Craig <craig.brentz@breaktech.com>
2017-03-16 20:17:57 -0500

    Parser checks director for response file

--- src/App/Controller/JSONParserController.php
+++ src/App/Controller/JSONParserController.php
@@ -6,8 +6,22 @@ use App\WatsonResults\WatsonResults;
 class JSONParserController {
 
   public $watsonResults;
+  public $path = "../../data";
 
   public function parseJSONFile($id) {
+
+    $files = scandir($this->path);
+    $contents = NULL;
+
+    foreach ($files as $dir) {
+      if (preg_match('({$id})\_', $dir)) {
+        $contents = file_get_contents('{$this->path}/{$dir}/{$id}_response.json');
+      }
+    }
+
+    if (!$contents) {
+      return false;
+    }
 
     $contents = file_get_contents("../examples/sample_data.txt");
     $jsonObjects = $this->splitIntoJSONObjects($contents);
