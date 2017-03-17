07a68f71c38efdcc4a081663a94fd34e38cc24ce
Mina Khalil <mina.ea.khalil@gmail.com>
2017-03-17 00:41:34 -0500

    Adding scripts

--- src/routes.php
+++ src/routes.php
@@ -10,3 +10,14 @@ $app->get('/meeting/[{id}]', '\App\Controller\MeetingController:get');
 //    // Render index view
 //    return $this->renderer->render($response, 'index.phtml', $args);
 //});
+$app->get('/transcribe/[{id}]', function ($request, $response, $args) {
+    return error_log('TRANSCRIBING'.PHP_EOL, 3, '/tmp/err.log');
+});
+
+$app->get('/parse/[{id}]', function ($request, $response, $args) {
+    return error_log('PARSING'.PHP_EOL, 3, '/tmp/err.log');
+});
+
+$app->get('/analyze/[{id}]', function ($request, $response, $args) {
+    return error_log('ANALYZING'.PHP_EOL, 3, '/tmp/err.log');
+});
